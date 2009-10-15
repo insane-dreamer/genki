22a8072b785ed748abcc3912dc68367858c76823
François Montel <insane.dreamer@yahoo.ie>
2009-10-15 20:51:38 +0000

    single post page design

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,16 @@ module PostsHelper
   def more_content?
     @posts.size == Post::DEFAULT_LIMIT
   end
+
+  def middle_nav_button
+    if params[:action] == "index" 
+      image = 'archives.gif'
+      path = archives_path
+    else
+      image = 'win.gif'
+      path = root_path
+    end
+    content_tag(:div, link_to(image_tag(image), path), :id => 'middleNavButton')
+  end
+
 end
