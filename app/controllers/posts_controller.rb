67c1b4e795e321b4098175e019a43ea6717f793c
François Montel <insane.dreamer@yahoo.ie>
2009-10-16 22:13:37 +0000

    ajax navigation for single posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,23 @@ class PostsController < ApplicationController
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {:include => [:approved_comments, :tags]}))
     @comment = Comment.new
   end
+
+  def show_previous
+    show_next_or_previous("previous", params[:id])
+  end
+
+  def show_next
+    show_next_or_previous("next", params[:id])
+  end
+
+  private 
+  
+  def show_next_or_previous(direction,id)
+    @post = direction == "previous" ? Post.find(id).previous : Post.find(id).next
+    render :update do |page|
+    	page.replace_html 'fullPost', :partial => 'post', :object => @post
+      page.replace_html 'arrows', single_post_navigation
+    end  
+  end
+
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,12 +5,28 @@ module PostsHelper
     @posts.size == Post::DEFAULT_LIMIT
   end
 
+  def single_post_navigation
+    navigation = previous_nav_button
+    navigation << middle_nav_button
+    navigation << next_nav_button
+  end
+
+  def previous_nav_button
+    arrow = @post.previous ? link_to_remote(image_tag('arrow_left.jpg', :class => 'arrow'), :url => previous_post_path(@post), :html => {:class => 'ico-prev'}) : "&nbsp;"
+    content_tag :div, arrow, :id => 'leftArrow'
+  end
+
+  def next_nav_button
+    arrow = @post.next ? link_to_remote(image_tag('arrow_right.jpg', :class => 'arrow'), :url => next_post_path(@post), :html => {:class => 'ico-next'}) : "&nbsp;"    
+    content_tag :div, arrow, :id => 'rightArrow'
+  end
+
   def middle_nav_button
     if params[:action] == "index" 
       image = 'archives.gif'
       path = archives_path
     else
-      image = 'win.gif'
+      image = @post.section.name + '.gif'
       path = root_path
     end
     content_tag(:div, link_to(image_tag(image), path), :id => 'middleNavButton')
