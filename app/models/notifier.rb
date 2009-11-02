5e78d6e91cfec4d385ad3d402cfa11368e8e3176
François Montel <insane.dreamer@yahoo.ie>
2009-11-02 23:21:43 +0000

    email for notifications

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,8 +1,8 @@
 class Notifier < ActionMailer::Base
   
   def user_submission(submission)
-    recipients "francis@ws.local"
-    from       "WTW site submissions <wtw@thefamily.org>" 
+    recipients "wtw@thefamily.org"
+    from       "WTW site submissions <wtw@wsfamily.com>" 
     subject    "Submission from: " + submission[:name]
     sent_on    Time.now
     body       submission
