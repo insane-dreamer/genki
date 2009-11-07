e1760713b730eca2f00a7ed7890fbb661dbe8039
François Montel <insane.dreamer@yahoo.ie>
2009-11-07 17:34:38 -0600

    display spinner during all ajax requests

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+Ajax.Responders.register({   
+  onCreate: function(){   
+    $('spinner').show();   
+  },   
+  onComplete: function() {   
+    if(Ajax.activeRequestCount == 0)   
+      $('spinner').hide();   
+  }   
+}); 
