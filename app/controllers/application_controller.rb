c1b645b9d96113ea68a9baf8ff0334b2eb950ff3
François Montel <insane.dreamer@yahoo.ie>
2009-11-21 15:23:28 -0600

    use hoptoad for errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include ExceptionNotifiable
+#  include ExceptionNotifiable # using hoptoad
 
   helper :all # include all helpers, all the time
 
