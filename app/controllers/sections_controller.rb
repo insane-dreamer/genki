566e255b7f05cb727d4ac4c0e96e5a9286019256
François Montel <insane.dreamer@yahoo.ie>
2009-11-23 21:55:19 +0000

    pagination for archives

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,8 +3,16 @@ class SectionsController < ApplicationController
   end
 
   def show
+    @page = params[:page] ? params[:page].to_i : 1
+    # the per_page corresponds to the number of months shown, not posts
+    per_page = 2
     @section = Section.find(params[:id])
-    @months = @section.posts.find_all_grouped_by_month
+    @posts = @section.posts.find_all_grouped_by_month
+    @months = @posts.paginate :page => @page, :per_page => per_page
+    # use these vars for custom pagination
+    # pages reversed since most recent is shown first
+    @previous = @page + 1 if !@posts.paginate(:page => @page + 1, :per_page => per_page).empty?
+    @next = @page - 1 if (!@posts.paginate(:page => @page - 1, :per_page => per_page).empty? if @page - 1 > 0)
   end
 
 end
