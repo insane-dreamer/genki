7a99996c567b7c64cff607e062381877bc12c623
François Montel <insane.dreamer@yahoo.ie>
2009-11-26 02:28:24 +0000

    allow non-image uploads, but don't create thumbs on them

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,10 +1,17 @@
 class Upload < ActiveRecord::Base
-  has_attached_file :file, :styles => { :original => "x640>", :thumb => "100>" }
+  has_attached_file :file, :styles => { :original => "x640>", :thumb => "100>" }, :whiny => false
+  validates_attachment_content_type :file, :content_type => ['image/jpeg','image/png','image/gif','text/rtf','text/html',"application/pdf", "application/zip", "application/msword"]
+  validates_attachment_size :file, :in => (1..2.megabytes)
+  before_file_post_process :check_for_image
 
   named_scope :images, :conditions => [ 'file_content_type LIKE ?', '%image%' ], :order => 'created_at DESC'
 
   def is_image?
     self.file.content_type.include?('image')
+  end
+
+  def check_for_image
+    return false unless self.is_image?
   end
 
 end
