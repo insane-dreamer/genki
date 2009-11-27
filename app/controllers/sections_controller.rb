f85673cc69ef9eda229681d47f8e23c5a1ad4a99
François Montel <insane.dreamer@yahoo.ie>
2009-11-27 23:21:13 +0000

    refactoring - don't need to send var to view

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -7,12 +7,12 @@ class SectionsController < ApplicationController
     # the per_page corresponds to the number of months shown, not posts
     per_page = 2
     @section = Section.find(params[:id])
-    @posts = @section.posts.published.find_all_grouped_by_month
-    @months = @posts.paginate :page => @page, :per_page => per_page
+    posts = @section.posts.published.find_all_grouped_by_month
+    @months = posts.paginate :page => @page, :per_page => per_page
     # use these vars for custom pagination
     # pages reversed since most recent is shown first
-    @previous = @page + 1 if !@posts.paginate(:page => @page + 1, :per_page => per_page).empty?
-    @next = @page - 1 if (!@posts.paginate(:page => @page - 1, :per_page => per_page).empty? if @page - 1 > 0)
+    @previous = @page + 1 if !posts.paginate(:page => @page + 1, :per_page => per_page).empty?
+    @next = @page - 1 if (!posts.paginate(:page => @page - 1, :per_page => per_page).empty? if @page - 1 > 0)
   end
 
 end
