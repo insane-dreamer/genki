2061bd3f73846dfd5ef1c644c3b86fd3ac044563
François Montel <insane.dreamer@yahoo.ie>
2009-12-09 01:27:27 +0000

    no 'tweet' section; handled differently

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 Section.create(:name => "win", :per_page => 1)
 Section.create(:name => "leap", :per_page => 2)
 Section.create(:name => "think", :per_page => 2)
-Section.create(:name => "tweet", :per_page => 5)
 Page.create(:title => 'About this site', :slug => 'about', :body => 'Something about this site.')
