6fab6509542db7abcf39bec21ee688b653f0ddfb
François Montel <insane.dreamer@yahoo.ie>
2009-12-17 00:09:56 +0000

    manage sections in admin

--- app/controllers/admin/sections_controller.rb
+++ app/controllers/admin/sections_controller.rb
@@ -1,20 +1,53 @@
-class Admin::SectionsController < ApplicationController
+class Admin::SectionsController < Admin::BaseController
+
+before_filter :find_section, :only => [:show, :update, :destroy]
+
   def index
+    @sections = Section.paginate :order => "created_at DESC", :page  => params[:page]
   end
 
   def new
+    @section = Section.new
+    render :action => :show
   end
 
-  def create
+  def show
+  end
+
+  def create  
+    if Section.create(params[:section])
+      flash[:notice] = "Section created."
+      redirect_to :action => :index
+    else
+      redirect_to :action => :new
+    end
   end
 
   def edit
+    render :action => :show
   end
 
   def update
+    if @section.update_attributes(params[:section])
+      flash[:notice] = "Section updated."
+      redirect_to :action => :index
+    else
+      redirect_to :action => :edit, :id => params[:id]
+    end
   end
 
   def destroy
+    if @section.destroy
+      flash[:notice] = "Section deleted."
+      redirect_to :action => :index
+    end    
   end
+
+  protected 
+
+  def find_section
+    @section = Section.find(params[:id])
+  end
+
 
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -4,7 +4,16 @@ has_many :posts
 
 validates_presence_of :name, :message => "can't be blank"
 validates_presence_of :per_page, :message => "can't be blank"
+validates_numericality_of :per_page, :only_integer => true, :message => "can only be whole number."
+validates_inclusion_of :per_page, :in => 1..2, :message => "can only be between 1 and 2."
 
 named_scope :show_on_front, :conditions => ['frontpage = ?', true]
+
+
+  def description_excerpt
+    excerpt = self.description ? self.description[0..50] : ''
+    excerpt + '...' if excerpt.length > 51
+  end
+  
 
 end
