51d812425b1d38c91ee6fc90313387ecf39fc3c4
François Montel <insane.dreamer@yahoo.ie>
2010-04-13 20:30:00 +0000

    ok, added additional mime types for IE

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,6 @@
 class Upload < ActiveRecord::Base
   has_attached_file :file, :styles => { :original => "x640>", :thumb => "100>" }, :whiny => false
-#  validates_attachment_content_type :file, :content_type => ['image/jpeg','image/png','image/gif','text/rtf','text/html',"application/pdf", "application/zip", "application/msword", "application/vnd.ms-excel","application/octet-stream"]
+  validates_attachment_content_type :file, :content_type => ['image/jpeg','image/pjpeg','image/png','image/x-png','image/gif','text/rtf',"text/richtext",'text/html',"application/pdf", "application/zip", "application/msword", "application/vnd.ms-excel","application/x-zip-compressed"]
   validates_attachment_size :file, :in => (1..200.megabytes)
   before_file_post_process :check_for_image
 
