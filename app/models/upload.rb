4d6b446ce13a3ef0cc1acf788b860b3499e6c770
François Montel <insane.dreamer@yahoo.ie>
2010-04-13 20:11:22 +0000

    testing to allow unknown file type

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,6 @@
 class Upload < ActiveRecord::Base
   has_attached_file :file, :styles => { :original => "x640>", :thumb => "100>" }, :whiny => false
-  validates_attachment_content_type :file, :content_type => ['image/jpeg','image/png','image/gif','text/rtf','text/html',"application/pdf", "application/zip", "application/msword", "application/vnd.ms-excel"]
+  validates_attachment_content_type :file, :content_type => ['image/jpeg','image/png','image/gif','text/rtf','text/html',"application/pdf", "application/zip", "application/msword", "application/vnd.ms-excel","application/octet-stream"]
   validates_attachment_size :file, :in => (1..10.megabytes)
   before_file_post_process :check_for_image
 
