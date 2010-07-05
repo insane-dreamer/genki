da66d7f1dc489eab0d4a3573a0f7b6ca520759d6
François Montel <insane.dreamer@yahoo.ie>
2010-07-05 23:27:35 +0000

    better logger.info output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,14 @@ class ApplicationController < ActionController::Base
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery :only => [:update, :destroy] # :secret => 'a6a9e417376364b61645d469f04ac8cf'
 
+
+  def log(*entries)
+    # used to preceed logger output to make it easier to find
+    logger.info "\e[1;32m LOGGER OUTPUT: #{Time.now} \e[0m"
+    entries.each { |e| logger.info "\e[1;31m #{e}\e[0m" } 
+    logger.info "\e[1;32m --- END --- \e[0m"
+  end
+
   protected
 
   def set_content_type
