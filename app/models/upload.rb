0fd8fb2d5db94f68204782213f7b95a1d33ace3f
Francis Montel <insane.dreamer@yahoo.ie>
2010-12-10 02:18:43 +0000

    have an option for larger thumbnails

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,5 @@
 class Upload < ActiveRecord::Base
-  has_attached_file :file, :styles => { :original => "x640>", :thumb => "100>" }, :whiny => false
+  has_attached_file :file, :styles => { :original => "x640>", :thumb => "100>", :medium => "160>" }, :whiny => false
   validates_attachment_content_type :file, :content_type => ['image/jpeg','image/pjpeg','image/png','image/x-png','image/gif','text/rtf',"text/richtext",'text/html',"application/pdf", "application/zip", "application/msword", "application/vnd.ms-excel","application/x-zip-compressed"]
   validates_attachment_size :file, :in => (1..200.megabytes)
   before_file_post_process :check_for_image
